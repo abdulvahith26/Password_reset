4129ec3b4829c1ef2430aff64300d197967f017f
abdulvahith26 <abdulvahith2023@gmail.com>
2024-12-01 14:17:18 +0530

    update router

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,3 +1,4 @@
+import React from 'react';
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import HomePage from "./pages/HomePage";
 import RegisterPage from "./pages/RegisterPage";
@@ -5,18 +6,24 @@ import ForgotPage from "./pages/ForgotPage";
 import Resetpage from "./pages/Resetpage";
 
 
-function App() {
-  
+
+const App = () => {
   return (
-    <Router>
+    <div>
+      <Router>
         <Routes>
           <Route path="/" element={< HomePage/>} />
           <Route path="/register" element={ <RegisterPage/>} />
           <Route path="/forgotpassword" element ={<ForgotPage/>  } />
           <Route path="/resetpassword/:token" element ={ <Resetpage />}  />
         </Routes>
-    </Router> 
+    </Router>
+
+
+
+    </div>
   );
-}
+};
 
 export default App;
+
